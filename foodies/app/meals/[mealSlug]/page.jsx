79e078db03dc01293aca808ecaa181cf148b5406
Nick Edwards <nledwa01@gmail.com>
2026-05-14 10:51:58 -0500

    Completed videos 473-475

--- foodies/app/meals/[mealSlug]/page.jsx
+++ foodies/app/meals/[mealSlug]/page.jsx
@@ -4,8 +4,23 @@ import { notFound } from 'next/navigation';
 import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
 
-export default function MealsDetailsPage({ params }) {
-    const meal = getMeal(params.mealSlug);
+export async function generateMetadata({ params }) {
+    const { mealSlug } = await params;
+    const meal = getMeal(mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+
+    return {
+        title: meal.title,
+        description: meal.summary,
+    };
+}
+
+export default async function MealsDetailsPage({ params }) {
+    const { mealSlug } = await params;
+    const meal = getMeal(mealSlug);
 
     if (!meal) {
         notFound();
